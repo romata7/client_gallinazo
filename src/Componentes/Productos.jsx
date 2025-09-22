461307b6ddc5c1adba3265b9f1566051ccbcc2b9
romata7 <41317423+romata7@users.noreply.github.com>
2025-09-21 20:04:43 -0500

    commit desde puerto

--- src/Componentes/Productos.jsx
+++ src/Componentes/Productos.jsx
@@ -1,22 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import API_BASE_URL from "../config";
-import { Modal, Button, FloatingLabel, Form, ListGroup } from "react-bootstrap";
+import { Modal, Button, FloatingLabel, Form, ListGroup, Badge } from "react-bootstrap";
 import moment from "moment";
-import { ArrowDown, ArrowUp, Pencil, Trash } from "react-bootstrap-icons";
+import { ArrowDown, ArrowUp, Pencil, Plus, Trash } from "react-bootstrap-icons";
+import { ModalProductos } from "./productos/ModalProductos";
 
-const vacio = {
+const DEFAULT_PRODUCT_DATA = {
   producto: "",
   costo: ""
 }
 
 function Productos() {
-  const [showEditProduct, setShowEditProduct] = useState(false);
-  const [showDeleteProduct, setShowDeleteProduct] = useState(false);
-
-  const [data, setData] = useState(vacio);
   const [productos, setProductos] = useState([]);
   const [productos_historial, setProductos_historial] = useState([]);
+
+  const [modalState, setModalState] = useState({
+    show: false,
+    operation: 'Registrar',
+    initialData: DEFAULT_PRODUCT_DATA
+  });
 
   const fetchProductos = async () => {
     try {
@@ -28,68 +31,9 @@ function Productos() {
     }
   };
 
-  const agregarProducto = async () => {
-    try {
-      const response = await axios.post(`${API_BASE_URL}/api/productos`, data)
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   useEffect(() => {
     fetchProductos();
-  }, [data])
-
-  const handleOpenEdit = (pp) => {
-    setP_aux(pp);
-    setShowEditProduct(true);
-  };
-  const handleCloseEdit = () => {
-    setShowEditProduct(false);
-    setP_aux({ id_reg_producto: 0, producto: "", costo: 0 });
-  };
-  const handleConfirmEdit = async (pp) => {
-    setShowEditProduct(false);
-    try {
-      const response = await axios.post(
-        `${API_BASE_URL}/api/productos/modificar`,
-        {
-          producto: p_aux,
-        }
-      );
-      setP_p({ id_reg_producto: 0, producto: "", costo: 0 });
-      setP_aux({ id_reg_producto: 0, producto: "", costo: 0 });
-      fetchProductos();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleOpenDelete = (pp) => {
-    setP_aux(pp);
-    setShowDeleteProduct(true);
-  };
-  const handleCloseDelete = () => {
-    setShowDeleteProduct(false);
-    setP_aux({ id_reg_producto: 0, producto: "", costo: 0 });
-  };
-  const handleConfirmDelete = async (pp) => {
-    setShowDeleteProduct(false);
-    try {
-      const response = await axios.post(
-        `${API_BASE_URL}/api/productos/eliminar`,
-        {
-          producto: p_aux,
-        }
-      );
-      setP_p({ id_reg_producto: 0, producto: "", costo: 0 });
-      setP_aux({ id_reg_producto: 0, producto: "", costo: 0 });
-      fetchProductos();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  }, []);
 
   const subirOrden = async (p) => {
     try {
@@ -104,318 +48,134 @@ function Productos() {
     console.log('bajar', p)
   }
 
-  const handleChange = (e) => {
-    let { name, value } = e.target
-    setData(prev => ({ ...prev, [name]: value }));
-  }
+  const handelOpenModal = useCallback((
+    operation,
+    productData = DEFAULT_PRODUCT_DATA
+  ) => {
+    setModalState({
+      show: true,
+      operation,
+      initialData: productData
+    })
+  }, []);
+
+  const handelCloseModal = useCallback(() => {
+    setModalState(prev => ({
+      ...prev,
+      show: false
+    }));
+  }, [])
+
+  const handleModalData = useCallback(async (modalData) => {
+    try {
+      let response;
+      let id
+      switch (modalState.operation) {
+        case "Registrar":
+          response = await axios.post(`${API_BASE_URL}/api/productos`, modalData)
+          break;
+        case "Modificar":
+          id = modalData;
+          response = await axios.put(`${API_BASE_URL}/api/productos/${id}`, modalData)
+          break;
+        case "Eliminar":
+          id = modalData;
+          response = await axios.delete(`${API_BASE_URL}/api/productos/${id}`, modalData)
+          break;
+
+        default:
+          break;
+      }
+      if (response.data) {
+        setProductos(response.data.productos || productos);
+        setProductos_historial(response.data.productos_historial || productos_historial);
+      }
+      handelCloseModal();
+    } catch (error) {
+      console.error(error);
+    }
+  })
 
   return (
-    <>
-      <div>
-        <div className="row p-2">
-          <div className="">
-            <div className="d-flex flex-column gap-2">
-              <FloatingLabel controlId="producto" label="Producto">
-                <Form.Control
-                  type='text'
-                  name="producto"
-                  value={data.producto}
-                  onChange={handleChange}
-                  placeholder=""
-                  autoFocus
-                />
-              </FloatingLabel>
-              <FloatingLabel controlId="costo" label="Costo (S/)">
-                <Form.Control
-                  type="number"
-                  step={0.01}
-                  name="costo"
-                  value={data.costo}
-                  onChange={handleChange}
-                />
-              </FloatingLabel>
-            </div>
-            <div className="d-flex gap-2 justify-content-evenly my-2">
-              <Button
-                size="sm"
-                variant="warning"
-                onClick={() => setData(vacio)}
-              >
-                Limpiar
-              </Button>
-              {data.costo > 0 && data.producto !== "" && (
-
+    <div>
+      <div
+        className="d-flex justify-content-center align-items-center my-2"
+      >
+        <Button
+          onClick={() => handelOpenModal("Registrar")}
+          size="sm"
+        >
+          <Plus /> Agregar Producto
+        </Button>
+      </div>
+      {productos.length > 0 && (
+        <ListGroup>
+          {productos.map((item) => (
+            <ListGroup.Item key={item.id}
+              className="d-flex gap-2"
+            >
+              <div className="d-flex gap-2">
                 <Button
                   size="sm"
-                  variant="primary"
-                  onClick={agregarProducto}
+                  variant="outline-primary"
+                  onClick={() => subirOrden(p)}
                 >
-                  Agregar
+                  <ArrowUp />
                 </Button>
-              )}
-            </div>
-          </div>
-          {productos.length > 0 && (
-            <ListGroup>
-              {productos.map((item) => (
-                <ListGroup.Item key={item.id}
-                  className="d-flex gap-2"
+                <Button
+                  size="sm"
+                  variant="outline-secondary"
+                  onClick={() => bajarOrden(p)}
                 >
+                  <ArrowDown />
+                </Button>
+              </div>
+              <div className="flex-grow-1">
+                <div>
                   <div className="d-flex gap-2">
-                    <Button
-                      size="sm"
-                      variant="outline-primary"
-                      onClick={() => subirOrden(p)}
-                    >
-                      <ArrowUp />
-                    </Button>
-                    <Button
-                      size="sm"
-                      variant="outline-secondary"
-                      onClick={() => bajarOrden(p)}
-                    >
-                      <ArrowDown />
-                    </Button>
-                  </div>
-                  <div className="d-flex flex-grow-1">
-                    <div className="d-flex gap-2">
-                      <div>
-                        {item.orden}
-                      </div>
-                      <div className="flex-grow-1">
-                        {item.producto}
-                      </div>
-                      <div>
-                        {item.costo}
-                      </div>
+                    <div>
+                      {item.orden}
                     </div>
+                    <div className="flex-grow-1">
+                      {item.producto}
+                    </div>
+                    <div>
+                      <Badge>
+                        S/ {parseFloat(item.costo).toFixed(2)}
+                      </Badge>
+                    </div>
+                  </div>
+                </div>
 
-                  </div>
-                  <div className="d-flex gap-2">
-                    <Button
-                      size="sm"
-                      variant="warning"
-                      onClick={() => handleOpenEdit(p)}
-                    >
-                      <Pencil />
-                    </Button>
-                    <Button
-                      size="sm"
-                      variant="danger"
-                      onClick={() => handleOpenDelete(p)}
-                    >
-                      <Trash />
-                    </Button>
-                  </div>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-          )}
-          <div className="col-12 col-sm-12 col-md-8 col-lg-9 col-xl-9 col-xxl-10">
-            <table className="table table-sm">
-              <thead className="table-dark">
-                <tr>
-                  <th>Orden</th>
-                  <th>#</th>
-                  <th>Producto</th>
-                  <th style={{ textAlign: "right" }}>Costo</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {productos.map((p) => (
-                  <tr key={p.id_reg_producto}>
-                    <td>
-                      <div className="d-flex gap-2 justify-content-start">
-                        <Button
-                          size="sm"
-                          variant="outline-primary"
-                          onClick={() => subirOrden(p)}
-                        >
-                          <ArrowUp />
-                        </Button>
-                        <Button
-                          size="sm"
-                          variant="outline-secondary"
-                          onClick={() => bajarOrden(p)}
-                        >
-                          <ArrowDown />
-                        </Button>
-                      </div>
-                    </td>
-                    <td className="fw-bold">
-                      {p.origen === 0 ? (
-                        <>{p.id_reg_producto}</>
-                      ) : (
-                        <>
-                          <del>{p.origen}</del> {p.id_reg_producto}
-                        </>
-                      )}
-                    </td>
-                    <td>{p.producto}</td>
-                    <td style={{ textAlign: "right" }}>S/ {p.costo}</td>
-                    <td>
-                      <div className="d-flex gap-2 justify-content-end">
-                        <Button
-                          size="sm"
-                          variant="warning"
-                          onClick={() => handleOpenEdit(p)}
-                        >
-                          <Pencil />
-                        </Button>
-                        <Button
-                          size="sm"
-                          variant="danger"
-                          onClick={() => handleOpenDelete(p)}
-                        >
-                          <Trash />
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-        <div className="row p-2">
-          <div className="col">
-            <table className="table table-sm">
-              <thead className="table-dark">
-                <tr>
-                  <th>#</th>
-                  <th>Fecha</th>
-                  <th></th>
-                  <th>Producto</th>
-                  <th style={{ textAlign: "right" }}>Costo</th>
-                </tr>
-              </thead>
-              <tbody>
-                {productos_historial.map((p) => (
-                  <tr key={p.id_reg_producto}>
-                    <td className="fw-bold">
-                      {p.origen === 0 ? (
-                        <>{p.id_reg_producto}</>
-                      ) : (
-                        <>
-                          <del>{p.origen}</del> {p.id_reg_producto}
-                        </>
-                      )}
-                    </td>
-                    <td>{moment(p.fecha).format("YYYY-MM-DD HH:mm:ss")}</td>
-                    <td
-                      className={
-                        p.operacion === "Modificado"
-                          ? "text-primary"
-                          : p.operacion === "Eliminado"
-                            ? "text-danger"
-                            : ""
-                      }
-                    >
-                      {p.operacion}
-                    </td>
-                    <td>{p.producto}</td>
-                    <td style={{ textAlign: "right" }}>S/ {p.costo}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-      {showEditProduct && (
-        <Modal show={true} onHide={handleCloseEdit}>
-          <Modal.Body>
-            <div className="form-floating mb-2">
-              <input
-                type="text"
-                className="form-control"
-                placeholder=""
-                value={p_aux.producto}
-                onChange={(e) =>
-                  setP_aux((prev) => ({ ...prev, producto: e.target.value }))
-                }
-              />
-              <label htmlFor="floatingInput">Producto</label>
-            </div>
-            <div className="form-floating mb-2">
-              <input
-                type="number"
-                min={0}
-                step={0.01}
-                className="form-control"
-                placeholder=""
-                value={p_aux.costo}
-                onChange={(e) =>
-                  setP_aux((prev) => ({
-                    ...prev,
-                    costo: parseFloat(e.target.value) || 0,
-                  }))
-                }
-              />
-              <label htmlFor="floatingInput">Costo (S/)</label>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseEdit}>
-              Cerrar
-            </Button>
-            {p_aux.producto !== "" && p_aux.costo > 0 && (
-              <Button variant="warning" onClick={handleConfirmEdit}>
-                Modificar Producto
-              </Button>
-            )}
-          </Modal.Footer>
-        </Modal>
+              </div>
+              <div className="d-flex gap-2">
+                <Button
+                  size="sm"
+                  variant="warning"
+                  onClick={() => handelOpenModal("Modificar", item)}
+                >
+                  <Pencil />
+                </Button>
+                <Button
+                  size="sm"
+                  variant="danger"
+                  onClick={() => handelOpenModal("Eliminar", item)}
+                >
+                  <Trash />
+                </Button>
+              </div>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       )}
-      {showDeleteProduct && (
-        <Modal show={true} onHide={handleCloseDelete}>
-          <Modal.Body
-            style={{ pointerEvents: "none", color: "gray", opacity: "0.7" }}
-          >
-            <div className="form-floating mb-2">
-              <input
-                type="text"
-                className="form-control"
-                placeholder=""
-                value={p_aux.producto}
-                onChange={(e) =>
-                  setP_aux((prev) => ({ ...prev, producto: e.target.value }))
-                }
-              />
-              <label htmlFor="floatingInput">Producto</label>
-            </div>
-            <div className="form-floating mb-2">
-              <input
-                type="number"
-                min={0}
-                step={0.01}
-                className="form-control"
-                placeholder=""
-                value={p_aux.costo}
-                onChange={(e) =>
-                  setP_aux((prev) => ({
-                    ...prev,
-                    costo: parseFloat(e.target.value) || 0,
-                  }))
-                }
-              />
-              <label htmlFor="floatingInput">Costo (S/)</label>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseDelete}>
-              Cerrar
-            </Button>
-            {p_aux.producto !== "" && p_aux.costo > 0 && (
-              <Button variant="danger" onClick={handleConfirmDelete}>
-                Eliminar Producto
-              </Button>
-            )}
-          </Modal.Footer>
-        </Modal>
-      )}
-    </>
+      <ModalProductos
+        show={modalState.show}
+        operation={modalState.operation}
+        initialData={modalState.initialData}
+        handleClose={handelCloseModal}
+        handleSubmit={handleModalData}
+      />
+    </div>
   );
 }
 
---
+++ src/Componentes/productos/ModalProductos.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react"
+import { Button, FloatingLabel, Form, Modal } from "react-bootstrap"
+
+const variant = {
+    "Registrar": "primary",
+    "Modificar": "warning",
+    "Eliminar": "danger"
+}
+
+const DEFAULT_DATA = {
+    producto: "",
+    costo: ""
+}
+
+export const ModalProductos = ({
+    operation = "Registrar",
+    show,
+    handleClose,
+    initialData,
+    handleSubmit
+}) => {
+    const [data, setData] = useState(initialData);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData(prev => ({ ...prev, [name]: value }));
+    }
+
+    useEffect(() => {
+        if (show) {
+            setData(initialData || DEFAULT_DATA);
+        }
+    }, [show, initialData])
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>{operation} Producto</Modal.Title>
+            </Modal.Header>
+            <fieldset disabled={operation === "Eliminar"}>
+                <Modal.Body
+                    className="d-flex flex-column gap-2"
+                >
+                    <FloatingLabel controlId="producto" label="Productos">
+                        <Form.Control
+                            type="text"
+                            name="producto"
+                            value={data.producto}
+                            onChange={handleChange}
+                            placeholder=""
+                            autoFocus
+                        />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="costo" label="Costo (S/)">
+                        <Form.Control
+                            type="number"
+                            step={0.01}
+                            name="costo"
+                            value={data.costo}
+                            onChange={handleChange}
+                            placeholder=""
+                        />
+                    </FloatingLabel>
+                </Modal.Body>
+            </fieldset>
+            <Modal.Footer>
+                <Button
+                    variant="secondary"
+                    onClick={handleClose}
+                >
+                    Cerrar
+                </Button>
+                {operation === "Registrar" || operation === "Modificar"
+                    ? data.producto !== "" && data.costo > 0 && (
+                        <Button
+                            variant={variant[operation]}
+                            onClick={() => handleSubmit(data)}
+                        >
+                            {operation}
+                        </Button>
+                    )
+                    : <Button
+                        variant={variant[operation]}
+                        onClick={() => handleSubmit(data)}
+                    >
+                        {operation}
+                    </Button>
+                }
+            </Modal.Footer>
+        </Modal>
+    )
+}
